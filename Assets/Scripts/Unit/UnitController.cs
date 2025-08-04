92f5b11f509e386f925c577e88c190666a82f7a2
doncici77 <pkh2956@naver.com>
2025-08-04 16:13:12 +0900

    에셋 추가, 애니메이션 추출, 유닛 컨트롤러 클래스 생성

---
+++ Assets/Scripts/Unit/UnitController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class UnitController : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
