7683f32e0f3f77c1074a284fcda1d744048dd284
doncici77 <pkh2956@naver.com>
2025-08-04 16:59:11 +0900

    TMP 추가, 움직임 및 애니메이션 코드 추가

--- Assets/Scripts/Unit/UnitController.cs
+++ Assets/Scripts/Unit/UnitController.cs
@@ -2,15 +2,46 @@ using UnityEngine;
 
 public class UnitController : MonoBehaviour
 {
-    [SerializeField] private Animator _animator;
+    [SerializeField] private float _moveSpeed = 1.0f;
+    private Animator _animator;
+    private bool _isMoveCommanded = false;
+    private bool _isAttacking = false;
 
-    void Start()
+    private void Awake()
     {
-        
+        _animator = GetComponent<Animator>();
     }
 
-    void Update()
+    private void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        if (_isMoveCommanded && !_isAttacking)
+        {
+            transform.position += Vector3.right * _moveSpeed * Time.deltaTime;
+        }
+    }
+
+    /// <summary>
+    /// 유닛의 움직임 상태 함수
+    /// </summary>
+    /// <param name="isMoving">유닛을 움직이겠나?</param>
+    public void SetMoveState(bool isMoving)
+    {
+        _animator.SetBool("IsMove", isMoving);
+        _isMoveCommanded = isMoving;
+    }
+
+    /// <summary>
+    /// 유닛의 공격 상태 함수
+    /// </summary>
+    /// <param name="isAttacking">유닛이 공격을 하겠는가?</param>
+    public void SetAttackState(bool isAttacking)
+    {
+        _animator.SetBool("IsAttack", isAttacking);
+        _isAttacking = isAttacking;
     }
 }
