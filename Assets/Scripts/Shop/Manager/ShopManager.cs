eed0f74c02113860fd971a1d41227d0c6e554e0e
Taewan2Da <ncs0801@naver.com>
2025-08-09 18:25:42 +0900

    상점 Ui 조절 및 증축 코드 구현
    상점 Ui 조절
    증축 코드 구현
    증축 버튼 누르면 돈 지불 후 증축 가능
    증축은 최대 4번 가능 이후 막아놓음
    증축 비용 배열에 저장

--- Assets/Scripts/Shop/Manager/ShopManager.cs
+++ Assets/Scripts/Shop/Manager/ShopManager.cs
@@ -6,11 +6,23 @@ public class ShopManager : MonoBehaviour
     public GoldManager goldManager;
     public CooldownManager cooldownManager;
 
+    // 증축 비용을 담는 배열
+    private int[] additionCosts = { 1000, 3000, 5000, 8000 };
+    // 현재 증축 횟수
+    private int currentAdditions = 0;
+    private int maxAdditions = 4;
+
     private Dictionary<int, float> _cooldownTimes = new Dictionary<int, float>()
     {
+        //Unit Shop 1
         { 15, 1.0f },
         { 25, 2.0f },
-        { 100, 3.0f }
+        { 100, 3.0f },
+
+        //Unit Shop 2
+        { 50, 1.5f },
+        { 85, 2.5f },
+        { 300, 3.5f },
     };
 
     public void OnUnitButton(int cost)
@@ -51,5 +63,43 @@ public class ShopManager : MonoBehaviour
         {
             Debug.Log("골드 부족으로 터렛 설치 불가");
         }
+
+        //isInstallingTurret = true;  // 설치 모드 진입
+    }
+
+    public void OnAdditionButton()
+    {
+        if (currentAdditions >= maxAdditions)
+        {
+            Debug.Log("더 이상 증축할 수 없습니다. 최대 증축 횟수에 도달했습니다.");
+            return;
+        }
+
+        // 현재 증축 단계에 맞는 비용을 가져옴
+        int cost = additionCosts[currentAdditions];
+
+        // 골드가 충분한지 확인
+        if (goldManager != null && goldManager.SpendGold(cost))
+        {
+            Debug.Log("증축 완료! 비용: " + cost + "골드");
+
+            //// 상훈's 터렛 증축 코드 필요
+            //if (turretManager != null)
+            //{
+            //    turretManager.BuildTurretSpot();
+            //}
+            currentAdditions++;
+
+        }
+        else
+        {
+            Debug.Log("골드 부족으로 증축 불가.");
+        }
     }
 }
+
+    //public void OnSellingButton(int cost)
+    //{
+    //    Debug.Log("판매 완료");
+    //}
+
