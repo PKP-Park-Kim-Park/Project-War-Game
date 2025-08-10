5460f17e29ed69259c2d2110d1dea63d038b7562
Taewan2Da <ncs0801@naver.com>
2025-08-10 16:50:26 +0900

    주석 제거 및 프리팹 오버라이드
    주석 제거 및 프리팹 오버라이드

--- Assets/Scripts/Shop/Manager/ShopManager.cs
+++ Assets/Scripts/Shop/Manager/ShopManager.cs
@@ -43,34 +43,6 @@ public class ShopManager : MonoBehaviour
         //_unitCostsAndIndices.Add(300, 5);
     }
 
-    //public void OnUnitButton(int cost)
-    //{
-    //    if (cooldownManager != null && !cooldownManager.IsOnCooldown())
-    //    {
-    //        if (goldManager != null && goldManager.SpendGold(cost))
-    //        {
-    //            if (_cooldownTimes.TryGetValue(cost, out float cooldownTime))
-    //            {
-    //                cooldownManager.StartCooldown(cooldownTime);
-    //                Debug.Log($"유닛 구매! {cost} 골드 소모.");
-    //            }
-    //            else
-    //            {
-    //                Debug.LogWarning($"비용 {cost}에 대한 쿨타임 설정이 없습니다.");
-    //            }
-    //        }
-    //        else
-    //        {
-    //            Debug.Log("골드 부족!");
-    //        }
-    //    }
-    //    else
-    //    {
-    //        Debug.Log("쿨다운 중이거나 쿨다운 매니저 미연결");
-    //    }
-    //}
-    // 버튼의 OnClick 이벤트에 연결할 함수 (비용만 받음)
-
     public void OnUnitButton(int cost)
     {
         if (cooldownManager != null && !cooldownManager.IsOnCooldown())
